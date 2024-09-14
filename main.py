a876715eb74f06c9f6c11202dbf5829414fd01ce
Hashara13 <hnethmin13@gmail.com>
2024-09-14 21:05:39 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from typing import List,Optional
+from uuid import UUID, uuid4,uuid5
 app=FastAPI()
+class Task(BaseModel):
+    id:Optional[UUID]=None
+    title:str
+    desc:Optional[str]
+    completedStatus:bool=False
+
+tasks=[]
+
+@app.post("/tasks/",response_model=Task)
+def add_task(task:Task):
+    task.id=uuid4()
+    tasks.append(task)
+    return task
+    
 
 @app.get("/")
 def read():
@@ -9,3 +25,5 @@ def read():
 if __name__=="__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    
+    
